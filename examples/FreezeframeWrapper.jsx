931776f48d92618c998bc4454a23ec79c3ee8893
Cameron Yick <cameron.yick@datadoghq.com>
2019-10-12 14:02:29 -0400

    Patch Freezeframe React Wrapper

--- examples/FreezeframeWrapper.jsx
+++ examples/FreezeframeWrapper.jsx
@@ -1,14 +1,14 @@
-// Example of how to use freezeframe-lite in a react 16.8+ project using hooks
-// This is quite similar to the wrapper that you could use for the main Freezeframe-js library too.
+// Example of how to use freezeframe-lite in a React 16.8+ project using hooks
+// This is quite similar to the wrapper that could be used for the main Freezeframe-js library too.
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import Freezeframe from 'freezeframe-lite';
+import Freezeframe from '../freezeframe-lite';
 
 const FreezeframeWrapper = props => {
   const imgRef = useRef();
   const freezeframe = useRef();
 
-  const { src, className } = props;
+  const { src, className = "" } = props;
 
   useEffect(() => {
     freezeframe.current = new Freezeframe(imgRef.current);
@@ -22,8 +22,8 @@ const FreezeframeWrapper = props => {
 };
 
 FreezeframeWrapper.propTypes = {
-  src: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired, // Should end with GIF!
+  className: PropTypes.string
 };
 
 export default FreezeframeWrapper;
